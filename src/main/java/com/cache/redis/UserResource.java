3c3c6aa03b9177bb9b4e4ce44770fe34c982baba
Karishma <karishma96a@gmail.com>
2021-08-29 20:48:39 +0530

    Update rest mapping

--- src/main/java/com/cache/redis/UserResource.java
+++ src/main/java/com/cache/redis/UserResource.java
@@ -2,10 +2,7 @@ package com.cache.redis;
 
 import com.cache.redis.model.User;
 import com.cache.redis.repo.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -19,21 +16,22 @@ public class UserResource {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/add/{id}/{name}")
-    public User add(@PathVariable("id") final String id,
-                    @PathVariable("name") final String name) {
-        userRepository.save(new User(id, name, 20000L));
+    @PostMapping("/{id}/{name}/{salary}")
+    public User addUser(@PathVariable("id") final String id,
+                        @PathVariable("name") final String name,
+                        @PathVariable("salary") final Long salary) {
+        userRepository.save(new User(id, name, salary));
         return userRepository.findById(id);
     }
 
-    @GetMapping("/update/{id}/{name}")
-    public User update(@PathVariable("id") final String id,
-                       @PathVariable("name") final String name) {
+    @PutMapping("/{id}/{name}")
+    public User updateUser(@PathVariable("id") final String id,
+                           @PathVariable("name") final String name) {
         userRepository.update(new User(id, name, 1000L));
         return userRepository.findById(id);
     }
 
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public Map<String, User> delete(@PathVariable("id") final String id) {
         userRepository.delete(id);
         return all();
